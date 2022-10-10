d72d5bce98410e6aca5c9c4a330260da9d34fc1e
555 <fetisovden@yandex.ru>
2022-10-10 17:58:05 +0300

    1st commit

---
+++ src/main/java/pro/sky/Spring_2_11/Spring_2_11.java
@@ -0,0 +1,13 @@
+package pro.sky.Spring_2_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_2_11 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring_2_11.class, args);
+	}
+
+}
